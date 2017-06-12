5c11f724f82fb9e17ddc700b00df735903c51055
Dave Olszewski <daveo@nodesource.com>
2017-06-11 22:00:13 -0700

    Revert "Problem: secure servers ignore zap_connect failre code and set ready."
    This reverts commit a0ccdc866efbac5889d8872c7f27c53e9c0a7c5b.

--- src/gssapi_server.cpp
+++ src/gssapi_server.cpp
@@ -120,21 +120,20 @@ int zmq::gssapi_server_t::process_handshake_command (msg_t *msg_)
 
     if (security_context_established) {
         //  Use ZAP protocol (RFC 27) to authenticate the user.
+        bool expecting_zap_reply = false;
         int rc = session->zap_connect ();
-        if (rc != 0)
-            return -1;
-        rc = send_zap_request ();
-        if (rc != 0)
-            return -1;
-        rc = receive_and_process_zap_reply ();
-        if (rc == 0)
-            state = send_ready;
-        else
-        if (errno == EAGAIN)
-            state = expect_zap_reply;
-        else
-            return -1;
-
+        if (rc == 0) {
+            rc = send_zap_request ();
+            if (rc != 0)
+                return -1;
+            rc = receive_and_process_zap_reply ();
+            if (rc != 0) {
+                if (errno != EAGAIN)
+                    return -1;
+                expecting_zap_reply = true;
+            }
+        }
+        state = expecting_zap_reply? expect_zap_reply: send_ready;
         return 0;
     }
 
